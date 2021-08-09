a9e5ee25914e52a773bc10366fb055a5b3da9db3
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-09 19:26:21 +0300

    verify the initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,13 +6,14 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const initialMessage = "Initial Contract Message";
 
 beforeEach(async () => {
     //get a list of all accounts
     accounts = await web3.eth.getAccounts();
     //use one of them to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ["Initial Contract Message"] })
+        .deploy({ data: bytecode, arguments: [initialMessage] })
         .send({ from: accounts[0], gas: '1000000' })
 }); 
 
@@ -20,4 +21,8 @@ describe('Inbox', () => {
     it('Deploy a contract', () => {
         assert.ok(inbox.options.address);
     });
+    it('Verify existence of the initial message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, initialMessage);
+    })
 })
