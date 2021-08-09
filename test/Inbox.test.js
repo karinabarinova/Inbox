3a561bb729a3b3a52845a5f322e2b218bf56c48c
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-09 18:07:02 +0300

    refactor to async/await, deployment with Web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,21 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //constructor function
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async () => {
     //get a list of all accounts
-    web3.eth.getAccounts()
-        .then(accounts => {
-            console.log('accounts', accounts);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+    accounts = await web3.eth.getAccounts();
     //use one of them to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Initial Contract Message"] })
+        .send({ from: accounts[0], gas: '1000000' })
 }); 
 
 describe('Inbox', () => {
     it('Deploy a contract', () => {
-        
+        console.log('inbox', inbox)
     });
 })
